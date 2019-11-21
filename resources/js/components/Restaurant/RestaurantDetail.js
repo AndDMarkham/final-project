cb829beef131b68e7d9412908d95448eccf26c90
Hel <helenahrubesova80@gmail.com>
2019-11-21 15:00:31 +0100

    rest details

--- resources/js/components/Restaurant/RestaurantDetail.js
+++ resources/js/components/Restaurant/RestaurantDetail.js
@@ -5,8 +5,16 @@ import "slick-carousel/slick/slick-theme.css";
 import DishForm from '../Dish/DishForm';
 
 const RestaurantDetail = props => {
+
+    const restaurantDetailStyle = { padding:'2rem'}
     return (
         <div>
+            <p style={{display:'flex', flexDirection:'row'}}>
+                <div style={restaurantDetailStyle}>Some restaurant</div>
+                <div style={restaurantDetailStyle}>Some address</div>
+                <div style={restaurantDetailStyle}>Some phone</div>
+                <div style={restaurantDetailStyle}>Some website_url</div>
+            </p>
             
             <DishForm
             />
